ca861550bc36e98e467f689a6b8822a0a9deb88a
narmatharv <59188021+narmatharv@users.noreply.github.com>
2022-04-18 21:28:54 -0500

    Fresh Spring boot

---
+++ first pring boot/first_spring_boot/src/test/java/com/example/first/springboot/first_spring_boot/FirstSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.first.springboot.first_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
